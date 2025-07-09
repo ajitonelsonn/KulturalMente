c27c01e227511d56a2783ec3875fd7de926b6df9
ajito nelson <8997ajito@gmail.com>
2025-07-09 10:53:57 +0900

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ["images.unsplash.com", "cdn.qloo.com"],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
